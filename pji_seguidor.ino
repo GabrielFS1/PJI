bd36b416455530a14a47b3b929978a6edb2127e5
Gabriel S <fernandesg.s@hotmail.com>
2022-08-20 22:18:32 -0300

    Update pji_seguidor.ino

--- pji_seguidor.ino
+++ pji_seguidor.ino
@@ -1,21 +1,23 @@
 #include <SoftwareSerial.h>
 
 // H bridge pins
-int IN2 = 4; // red
-int IN1 = 2; // brown
-int IN3 = 5; // green
-int IN4 = 7; // yellow
-int ENA = 3; // purple
-int ENB = 6; // blue
+int ENA = 3;
+int IN1 = 4;
+int IN2 = 5;
+
+int IN3 = 8;
+int IN4 = 7;
+int ENB = 6;
+
+int vel = 30;
 
 // Reflectance Sensors pins
-int sensor_left = 10;
-int sensor_center = 9;
-int sensor_right = 8;
+int sensor_left = 11
+;
+int sensor_center = 10;
+int sensor_right = 9;
 
 int left_val, center_val, right_val;
-
-int vel = 45;
 
 void setup()
 {
@@ -53,33 +55,30 @@ void loop()
   Serial.print(right_val);
   Serial.print("\n");
 
-  if ((left_val == 0) && (center_val == 0) && (right_val == 0))
+  if ((left_val == 0) && (center_val == 1) && (right_val == 0))
   {
+    set_vel(vel);
     forward();
-  }
-  else if ((left_val == 0) && (center_val == 1) && (right_val == 0))
-  {
-    forward();
-  }
-  else if ((left_val == 1) && (center_val == 1) && (right_val == 0))
-  {
-    right();
   }
   else if ((left_val == 1) && (center_val == 0) && (right_val == 0))
   {
+    set_vel(1.5 * vel);
     right();
   }
-  else if ((left_val == 0) && (center_val == 1) && (right_val == 1))
+  else if ((left_val == 1) && (center_val == 1) && (right_val == 0))
   {
-    left();
+    set_vel(1.5 * vel);
+    right();
   }
   else if ((left_val == 0) && (center_val == 0) && (right_val == 1))
   {
+    set_vel(1.5 * vel);
     left();
   }
-  else if ((left_val == 1) && (center_val == 1) && (right_val == 1))
+  else if ((left_val == 0) && (center_val == 1) && (right_val == 1))
   {
-    stop();
+    set_vel(1.5 * vel);
+    left();
   }
 }
 
@@ -101,18 +100,18 @@ void backward()
 
 void left()
 {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
 }
 
 void right()
 {
-  digitalWrite(IN1, LOW);
+  digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 }
 
 void stop()
@@ -121,4 +120,9 @@ void stop()
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, LOW);
+}
+void set_vel(int velocity)
+{
+  analogWrite(ENA, velocity);
+  analogWrite(ENB, velocity);
 }
